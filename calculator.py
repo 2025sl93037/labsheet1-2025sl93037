3f2a981ae2e0cb43e9c6d0953792c303f3b0a573
Rithick Joel <2025sl93037@wilp.bits-pilani.ac.in>
2026-04-03 17:05:02 +0530

    Add divide function

--- calculator.py
+++ calculator.py
@@ -1,8 +1,15 @@
 def add(a, b):
 return a + b
 
-def multiply(a, b)
+def multiply(a, b):
 return a * b
 
+<<<<<<< HEAD
 def subtract(a, b):
 return a - b
+=======
+def divide(a, b):
+if b == 0:
+return None
+return a / b
+>>>>>>> fd8b7cf (Add divide function)
