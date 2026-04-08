ce5fffdae547147e39c02c2b5e1226afb3095d8f
Rithick Joel <2025sl93037@wilp.bits-pilani.ac.in>
2026-04-08 12:01:12 +0530

    Added calulator

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 def add(a, b):
-return a + b
+	return a + b
 
 def multiply(a, b):
-return a * b
+	return a * b
 
-<<<<<<< HEAD
+
 def subtract(a, b):
-return a - b
-=======
+	return a - b
+
 def divide(a, b):
-if b == 0:
-return None
-return a / b
->>>>>>> fd8b7cf (Add divide function)
+	if b == 0:
+		return None
+	return a / b
+
